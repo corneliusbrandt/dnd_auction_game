510004eb5cef39688b9d4b527baa1dff57b2e556
corneliusbrandt <bc7970@mci4me.at>
2025-10-03 22:26:42 +0200

    Added first own agent

---
+++ example_agents/first_agent.py
@@ -0,0 +1,48 @@
+import numpy as np
+import random
+import os
+from dnd_auction_game import AuctionGameClient
+
+############################################################################################
+#
+# first_agent self developed
+#
+############################################################################################
+
+class FirstAgent:
+    def __init__(self):
+        pass
+
+    def max_bid(agent_id:str, current_round:int, states:dict, auctions:dict, prev_auctions:dict, bank_state:dict):
+        agent_state = states[agent_id]
+        current_gold = agent_state["gold"]
+        points = agent_state["points"]
+
+        next_round_gold_income = 0
+        if len(bank_state["gold_income_per_round"]) > 0:
+            next_round_gold_income = bank_state["gold_income_per_round"][0]   
+
+
+        return{}
+    
+
+
+if __name__ == "__main__":
+    
+    host = "localhost"
+    agent_name = "{}_{}".format(os.path.basename(__file__), random.randint(1, 1000))
+    player_id = "id_of_human_player"
+    port = 8000
+
+    game = AuctionGameClient(host=host,
+                                agent_name=agent_name,
+                                player_id=player_id,
+                                port=port)
+    agent = FirstAgent()
+    try:
+        game.run(agent.max_bid())
+    except KeyboardInterrupt:
+        print("<interrupt - shutting down>")
+
+    print("<game is done>")
+
